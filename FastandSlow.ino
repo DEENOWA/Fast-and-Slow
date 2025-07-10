3c9d048931fb4e65c9c4ea2d3894c90dafe3b2d0
Fatona Ayomide Tolani <ayomidefemi780@gmail.com>
2025-07-10 15:06:11 +0100

    Add files via upload

---
+++ FastandSlow.ino
@@ -0,0 +1,95 @@
+int redLed = 12;
+int yellowLed = 11;
+int slow = 500;
+int fast = 200;
+
+
+void setup() {
+  pinMode(redLed,OUTPUT);
+  pinMode(yellowLed,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+
+  digitalWrite(yellowLed,HIGH);
+  delay(fast);
+  digitalWrite(yellowLed,LOW);
+  delay(fast);
+
+  digitalWrite(yellowLed,HIGH);
+  delay(fast);
+  digitalWrite(yellowLed,LOW);
+  delay(fast);
+
+  digitalWrite(yellowLed,HIGH);
+  delay(fast);
+  digitalWrite(yellowLed,LOW);
+  delay(fast);
+
+  digitalWrite(yellowLed,HIGH);
+  delay(fast);bbb
+  digitalWrite(yellowLed,LOW);
+  delay(fast);
+
+  digitalWrite(yellowLed,HIGH);
+  delay(fast);
+  digitalWrite(yellowLed,LOW);
+  delay(fast);
+
+
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  digitalWrite(redLed,HIGH);
+  delay(slow);
+  digitalWrite(redLed,LOW);
+  delay(slow);
+
+  // put your main code here, to run repeatedly:
+
+}
